bf749e15dc8156cf71722ac32a5ebdd7501b6513
Jaemin Jeon <greedysiru@gmail.com>
2022-07-15 01:09:09 +0900

    REFACTOR : ReactDOM -> createRoot 교체

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
@@ -7,13 +7,15 @@ import { theme } from "./theme";
 
 const queryClient = new QueryClient();
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
-      </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    </QueryClientProvider>
+  </React.StrictMode>
 );
